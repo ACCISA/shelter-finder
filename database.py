1c430825fbd2f01df67605066235bbbfb943b642
richardbadir <ricobadir@hotmail.com>
2023-02-04 15:31:57 -0500

    checking if the inserted stuff is ok

--- database.py
+++ database.py
@@ -7,14 +7,17 @@ def Hash(word):
     hashed = hashlib.sha256(word.encode()).hexdigest()
     return hashed
 
-sqlite3.connect('shelter_finder.db')
+
 conn = sqlite3.connect('shelter_finder.db') 
 c = conn.cursor()
 
 
 def create_user(conn, user):
-    sql = ''' INSERT INTO user(username,password)
+    sql = ''' INSERT INTO user(username,password,email,shelter)
               VALUES(?,?) '''
+    for parameter in user:
+        if  isinstance(parameter,str):
+            raise Exception("a parameter in user is not a string")
     cur = conn.cursor()
     cur.execute(sql, user)
     conn.commit()
@@ -23,10 +26,39 @@ def create_user(conn, user):
 
 def create_shelter(conn, shelter):
 
-    sql = ''' INSERT INTO shelter(name,adress,email,telephone)
+    sql = ''' INSERT INTO shelter(name,adress,email,tel)
               VALUES(?,?,?,?) '''
+    
+    for parameter in shelter:
+        if  isinstance(parameter,str):
+            raise Exception("a parameter in shelter is not a string")
     cur = conn.cursor()
     cur.execute(sql, shelter)
+    conn.commit()
+    return cur.lastrowid
+
+def create_shelter_info(conn, shelter_info):
+
+    sql = ''' INSERT INTO shelter_info(shelter,shower,bed,food,therapist)
+              VALUES(?,?,?,?,?) '''
+    
+    if  isinstance(shelter_info[0],str):
+        raise Exception("shelter in shelter_info is not a string")
+
+    if  shelter_info[1] != 0 and shelter_info[1] != 1:
+        raise Exception("shower in shelter_info is not a 0 or 1")
+
+    if  shelter_info[2] != 0 and shelter_info[2] != 1:
+        raise Exception("bed in shelter_info is not a 0 or 1")
+
+    if  shelter_info[3] != 0 and shelter_info[3] != 1:
+        raise Exception("food in shelter_info is not a 0 or 1")
+
+    if  shelter_info[4] != 0 and shelter_info[4] != 1:
+        raise Exception("therapist in shelter_info is not a 0 or 1")
+
+    cur = conn.cursor()
+    cur.execute(sql, shelter_info[0], shelter_info[1], shelter_info[2], shelter_info[3])
     conn.commit()
     return cur.lastrowid
 
@@ -51,7 +83,8 @@ def create_database():
 
     c.execute('''
             CREATE TABLE IF NOT EXISTS shelter_info
-            ([shelter] VARCHAR(100) NOT NULL,
+            ([shelter_info_id] INTEGER PRIMARY KEY,
+            [shelter] VARCHAR(100) NOT NULL,
             [shower] BOOLEAN NOT NULL CHECK (shower IN (0,1)),
             [bed] BOOLEAN NOT NULL CHECK (bed IN (0,1)),
             [food] BOOLEAN NOT NULL CHECK (food IN (0,1)),
