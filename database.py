46f1ba98c1e2781f75527fb5a054bcdfdeebc3a7
AC <39713625+R0MULLUS@users.noreply.github.com>
2023-02-04 14:04:24 -0500

    database creation

--- database.py
+++ database.py
@@ -41,4 +41,34 @@ def create_shelter(conn, shelter):
     conn.commit()
     return cur.lastrowid
 
+def create_database():
+    c.execute('''
+            CREATE TABLE IF NOT EXISTS users
+            ([user_id] INTEGER PRIMARY KEY, 
+            [username] VARCHAR(110) NOT NULL, 
+            [password] VARCHAR(110) NOT NULL, 
+            [email] VARCHAR(50) NOT NULL,
+            [shelter] VARCHAR(50)) NOT NULL
+            ''')
+            
+    c.execute('''
+            CREATE TABLE IF NOT EXISTS shelters
+            ([shelter_id] INTEGER PRIMARY KEY, 
+            [name] VARCHAR(100) NOT NULL,
+            [adress] VARCHAR(100) NOT NULL,
+            [email] VARCHAR(100) NOT NULL,
+            [tel] VARCHAR(100) NOT NULL
+            )
+            ''')
+
+    c.execute('''
+            CREATE TABLE IF NOT EXISTS shelter_info
+            ([shelter] VARCHAR(100) NOT NULL,
+            [shower] BOOLEAN NOT NULL CHECK (shower IN (0,1)),
+            [bed] BOOLEAN NOT NULL CHECK (bed IN (0,1)),
+            [food] BOOLEAN NOT NULL CHECK (food IN (0,1)),
+            [therapist] BOOLEAN NOT NULL CHECK (therapist IN (0,1))
+            )
+            ''')      
+    conn.commit()
 
